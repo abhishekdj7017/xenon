919dfbe47fdc3a0c0a8f878926d7039bc1074609
abhishekdj7017 <105041594+abhishekdj7017@users.noreply.github.com>
2026-09-05 16:04:44 +0530

    Update meta-event.js

--- api/meta-event.js
+++ api/meta-event.js
@@ -3,8 +3,8 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const PIXEL_ID = "1809532570042392";
-  const ACCESS_TOKEN = EAANbFLf6MnsBSVvNywZCZB7A7pndeBDqInPHeLFKpZCULFIZB7QAI4NeVKk19LsclhXUQfaS7ve6ZA8SmITYTX0cr8CKMECrVomrj4DdOZC6iyp5cPlo2NuZBrfppxADBsOQwALZAQkCchjfdp1xPkROkfuL3tYPoXZCgFbWpB2QtBCuVGT2EmQD8fokf3RW4uxfZCNwZDZD;
+  const PIXEL_ID = "1605130227710024";
+  const ACCESS_TOKEN = EAAflsuPEZA94BSYhwoQNwtSjayOtvvU6GHVCZAVW8PNkh3mmDqwCgQFhpgESkg1BcYraNJgmeizMs9kCEyGQEUdD7qhFlcPMkcB3XWCRAoMfE1gk8RGKiCzB41YyEU3lhxH5FgWfgmyFwWEne2t4UxDpxUwKszyXyGd9dFkjgY8stdlwxMSlRiQ4aaa8fAuAZDZD;
 
   try {
     const response = await fetch(
